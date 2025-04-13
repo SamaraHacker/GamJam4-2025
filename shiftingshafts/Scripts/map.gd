f562ed2e03b1c9f6f22064b71842b5b4a3a83214
BigBoiRon <rgjelken@gmail.com>
2025-04-12 22:49:33 -0500

    Continued working on flipping feature

--- shiftingshafts/Scripts/map.gd
+++ shiftingshafts/Scripts/map.gd
@@ -1,34 +1,46 @@
 extends TileMap
 
-var boxLocation = 30
+var boxLocation = 0
 var rotationScalar = 0.005
-var maxRotation = 0
+var targetAngle = 0
+var prevCenter = 0
 var centerOfRotation = 0
 var dir = 1
+var currAngle = 0
+var offset = 0
+
+#var timer = 0
+var started = false
+
+var i = 0
+
+func _ready():
+	offset = box.position.x - position.x 
 
 @onready var box: RigidBody2D = $Box
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	currAngle = rotation_degrees
 	#make screen tilt based on box location
 	if centerOfRotation == 180:
 		dir = -1
 	
-	boxLocation = box.position.x*dir
-	maxRotation = (boxLocation/5)+centerOfRotation
-	if maxRotation > centerOfRotation+30:
-		maxRotation = centerOfRotation+30
-	elif maxRotation < centerOfRotation-30:
-		maxRotation = centerOfRotation-30
+	boxLocation = (box.position.x + offset)*dir
+	print(boxLocation)
+	targetAngle = (boxLocation/5)+centerOfRotation
+	print("Target Angle", targetAngle)
+	print("Actual Angle", (rotation_degrees))
 	
-	if abs(maxRotation-rotation_degrees ) > 50:
-		rotation_degrees += rotationScalar*200
-	elif not rotation_degrees == maxRotation:
-		if rotation_degrees>maxRotation:
-			rotation_degrees -= abs(boxLocation)*rotationScalar
+	targetAngle = clamp(targetAngle, centerOfRotation -30, centerOfRotation + 30)
+
+	if rotation_degrees != targetAngle:
+		if abs(rotation_degrees - targetAngle) > 20:
+			box.set_gravity_scale(0)
 		else:
-			rotation_degrees += abs(boxLocation)*rotationScalar
-	
-	
+			box.set_gravity_scale(0.5)
+		rotation = lerp_angle(rotation, deg_to_rad(targetAngle), 0.1)
+		#print(rotation_degrees)
+		#print(targetAngle)
 	#if rotation_degrees > maxRotation:
 	#	while rotation_degrees > maxRotation:
 	#		rotation_degrees -= abs(boxLocation)*rotationScalar
@@ -38,8 +50,9 @@ func _process(delta: float) -> void:
 	#elif rotation_degrees < maxRotation and rotation_degrees > maxRotation:
 	#	rotation_degrees += boxLocation*rotationScalar
 	
-	#box.rotation_degrees = rotation_degrees
-	box.global_rotation = global_rotation
+	#box.rotation = rotation
+	#print("Bot rotation", box.rotation)
+	#box.global_rotation = rotation
 	box.global_rotation_degrees = global_rotation_degrees		
 	
 	
