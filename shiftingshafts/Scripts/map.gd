841c4b693b8e8f25d41c18dc6ebbfb9afdc8c98c
BigBoiRon <rgjelken@gmail.com>
2025-04-13 14:27:55 -0500

    Updated map

--- shiftingshafts/Scripts/map.gd
+++ shiftingshafts/Scripts/map.gd
@@ -18,6 +18,7 @@ var j = 0
 
 func _ready():
 	offset =0 #+ position.x - box.position.x
+	
 
 @onready var box: RigidBody2D = $Box
 @onready var player: CharacterBody2D = $Player
@@ -29,10 +30,14 @@ func _physics_process(delta: float) -> void:
 	if centerOfRotation == 180:
 		dir = -1
 	
-	boxLocation = (box.position.x + offset)*dir
+	boxLocation = (box.position.x + offset) * dir
+	print("Box location", boxLocation)
 	targetAngle = (boxLocation/5)+centerOfRotation
 	
 	targetAngle = clamp(targetAngle, centerOfRotation -30, centerOfRotation + 30)
+	
+	if rotation_degrees < -31:
+		rotation_degrees += 360
 
 	if rotation_degrees != targetAngle:
 		if abs(rotation_degrees - targetAngle) > 120:
@@ -42,19 +47,20 @@ func _physics_process(delta: float) -> void:
 			#print("before", savedPosition)
 			started = true
 		elif (started):
-			if abs(rotation_degrees - targetAngle) < 20:
+			if abs(abs(rotation_degrees) - targetAngle) < 20:
+				print("Turned fully")
 				started = false
 				box.freeze = false
+				#box.position = Vector2(box.position.x, box.position.y-2)
 				#print("After", box.global_position)
-				var center = position  # TileMap's local origin
+				#var center = position  # TileMap's local origin
 				#box.position = (box.position - center).rotated(PI) + center
 			else:
 				player.velocity = Vector2.ZERO
 		rotation = lerp_angle(rotation, deg_to_rad(targetAngle), 0.05)
-	
+		print(rotation_degrees)
+		print(targetAngle)
 	
 	#print("Bot rotation", box.rotation)
 	box.global_rotation = global_rotation
 	#box.global_rotation_degrees = global_rotation_degrees		
-	
-	
