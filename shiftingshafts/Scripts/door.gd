a9ae05f4e667e8995918240b5b766153c2413bbe
SamaraHacker <samara28@iastate.edu>
2025-04-13 18:24:20 -0500

    Updated level and level selector

--- shiftingshafts/Scripts/door.gd
+++ shiftingshafts/Scripts/door.gd
@@ -15,5 +15,21 @@ func _on_body_entered(body):
 
 func _process(delta: float) -> void:
 	if win and winTimer < Time.get_ticks_msec():
-		get_tree().change_scene_to_file("res://Scenes/" + nextLevel + ".tscn")
+		if(nextLevel == "level_2"):
+			Global.completedLevel1 = true
+			get_tree().change_scene_to_file("res://Scenes/" + nextLevel + ".tscn")
+		if(nextLevel == "level_3"):
+			Global.completedLevel2 = true
+			get_tree().change_scene_to_file("res://Scenes/" + nextLevel + ".tscn")
+		if(nextLevel == "level_4"):
+			Global.completedLevel3 = true
+			get_tree().change_scene_to_file("res://Scenes/" + nextLevel + ".tscn")
+		if(nextLevel == "level_5"):
+			Global.completedLevel4 = true
+			get_tree().change_scene_to_file("res://Scenes/" + nextLevel + ".tscn")
+		if(nextLevel == "level_6"):
+			Global.completedLevel5 = true
+			get_tree().change_scene_to_file("res://Scenes/" + nextLevel + ".tscn")
+		if(nextLevel == "win_screen"):
+			get_tree().change_scene_to_file("res://Scenes/Menu/win_menu.tscn")
 	
